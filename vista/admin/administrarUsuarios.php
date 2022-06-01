86dc2ad3b0a0518a5c2ce3e8072a65dc21f7eb84
Juan Marcos Gonzalez <juan.gonzalez@est.fi.uncoma.edu.ar>
2022-06-01 02:29:21 -0300

    Listado de usuarios desde BD

--- vista/admin/administrarUsuarios.php
+++ vista/admin/administrarUsuarios.php
@@ -3,11 +3,6 @@ include_once '../../configuracion.php';
 
 $datos = data_submitted();
 
-if (!isset($datos["verificado"])) {
-    $controlIngresoAdmin = new controlIngresoAdmin();
-    $controlIngresoAdmin->verificarIngreso("administrarUsuarios");
-}
-
 $titulo = 'Administración de Usuarios';
 
 $abmUsuario = new AbmUsuario();
@@ -30,96 +25,25 @@ include_once '../estructura/header.php';
                 <table class="table align-middle">
                     <thead class="table-dark">
                         <tr>
-                            <th scope="col" class='text-center'>ID</th>
                             <th scope="col" class='text-center'>Razon Social</th>
                             <th scope="col" class='text-center'>DNI</th>
                             <th scope='col' class='text-center'>Genero</th>
                             <th scope="col" class='text-center'>Edad</th>
                         </tr>
                     </thead>
-
                     <tbody>
                         <?php
                         foreach ($listadoUsuarios as $usuario) {
-                            $id = $usuario->getIdUsuario();
-                            $abmUsuarioRol = new AbmUsuarioRol();
-                            $datos['idusuario'] = $id;
-                            $listaUsuarioRol = $abmUsuarioRol->buscar($datos);
-                            $rol = $listaUsuarioRol[0]->getObjRol()->getRolDescripcion(); ?>
+                        ?>
                             <tr>
-                                <td scope='row' class='text-center'><?php echo $id ?></td>
-                                <?php
-                                switch ($rol) {
-                                    case 'Admin': ?>
-                                        <td scope='row' class='text-center'><span class="badge rounded-pill bg-dark"><?php echo $rol ?></span></td>
-                                    <?php
-                                        break;
-                                    case 'Manager': ?>
-                                        <td scope='row' class='text-center'><span class="badge rounded-pill bg-success"><?php echo $rol ?></span></td>
-                                    <?php
-                                        break;
-                                    default: ?>
-                                        <td scope='row' class='text-center'><span class="badge rounded-pill bg-light text-dark"><?php echo $rol ?></span></td>
-                                <?php
-                                        break;
-                                }
-                                ?>
-
-                                <td scope='row' class='text-center'><?php echo $usuario->getUsNombre() ?></td>
-                                <td scope='row' class='text-center'><?php echo $usuario->getUsPass() ?></td>
-                                <td scope='row' class='text-center'><?php echo $usuario->getUsmail() ?></td>
-
-                                <?php
-                                $estado = $usuario->getUsdeshabilitado();
-                                if ($estado == "0000-00-00 00:00:00") {
-                                    $estado = "";
-                                }
-                                ?>
-
-                                <td scope='row'><?php echo $estado ?></td>
-
-                                <?php
-                                if ($id == $sesion->getIdUsuario()) { ?>
-                                    <td scope='row' class='text-center'></td>
-                                    <td scope='row' class='text-center'></td>
-                                    <td scope='row' class='text-center'></td>
+                                <td scope="row" class="text-center"><?php echo $usuario->getRazonSocial() ?></td>
+                                <td scope="row" class="text-center"><?php echo $usuario->getDni() ?></td>
+                                <td scope="row" class="text-center"><?php echo $usuario->getGenero() ?></td>
+                                <td scope="row" class="text-center"><?php echo $usuario->getEdad() ?></td>
+                            <?php
+                        }
+                            ?>
                             </tr>
-                        <?php
-                                } else { ?>
-                            <form method='post' action='actualizarUsuario.php'>
-                                <td class='text-center'>
-                                    <input name='idusuario' id='idusuario' type='hidden' value='<?php echo $id ?>'>
-                                    <button class='btn btn-warning btn-sm' type='submit' role='button'><i class='bi bi-pencil-square'></i>&nbsp;Editar</button>
-                                </td>
-                            </form>
-
-                            <form method='post' action='eliminarUsuario.php'>
-                                <td class='text-center'>
-                                    <input name='idusuario' id='idusuario' type='hidden' value='<?php echo $id ?>'>
-                                    <button class='btn btn-danger btn-sm' type='submit' value='<?php $id ?>' role='button'><i class='bi bi-trash'></i>&nbsp;Eliminar</button>
-                                </td>
-                            </form>
-
-
-                            <form method='post' action='deshabilitarUsuario.php'>
-                                <td class='text-center'>
-                                    <input name='idusuario' id='idusuario' type='hidden' value='<?php echo $id ?>'>
-                                    <?php
-                                    if ($usuario->getUsdeshabilitado() == "0000-00-00 00:00:00") { ?>
-                                        <button class='btn btn-secondary btn-sm' type='submit' value='<?php $id ?>' role='button'><i class='fas fa-ban'></i>&nbsp;Deshabilitar</button>
-                                    <?php
-                                    } else { ?>
-                                        <button class='btn btn-success btn-sm' type='submit' value='<?php $id ?>' role='button'><i class="fas fa-check-circle"></i>&nbsp;Habilitar</button>
-                                    <?php
-                                    }
-                                    ?>
-                                </td>
-                            </form>
-                            </tr>
-                    <?php
-                                }
-                            }
-                    ?>
                     </tbody>
                 </table>
             </div>
